56c38d4e7b51c23466641f3fee9427a806565c55
Kwang-Su Kim <atelier.jullian@gmail.com>
2018-11-21 01:40:12 +0000

    change directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,18 @@ var sass = require('gulp-sass');
 var concatCss = require('gulp-concat-css');
 
 gulp.task('serve', ['sass'], function() {
-  browserSync.init({server: "./public"});
-  gulp.watch("public/scss/*.scss", ['sass']);
-  gulp.watch("public/*.html").on('change', browserSync.reload);
+  browserSync.init({server: "./"});
+  gulp.watch("scss/*.scss", ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function() {
-  return gulp.src("public/scss/style.scss").pipe(sass()).pipe(gulp.dest("public/css")).pipe(browserSync.stream());
+  return gulp.src("scss/style.scss").pipe(sass()).pipe(gulp.dest("css")).pipe(browserSync.stream());
 });
 
 gulp.task('concat', function () {
-  return gulp.src('public/css/*.css')
-    .pipe(concatCss("public/css/style.css"))
+  return gulp.src('css/*.css')
+    .pipe(concatCss("css/style.css"))
     .pipe(gulp.dest('out/'));
 });
 
