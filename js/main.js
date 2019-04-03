d9021110c9c19bc9ad130b3e2621df409d181ef8
Kwang-Su Kim <atelier.jullian@gmail.com>
2019-04-03 03:36:14 +0100

    refresh and rebuild the page when it's resizing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    $(window).bind('resize', function (e) {
+        if (window.RT) clearTimeout(window.RT);
+        window.RT = setTimeout(function () {
+            this.location.reload(false);
+        }, 100);
+    });
+
     $(".menu").click(function() {
         if ($(".menu--container").hasClass("state")) {
             $(".menu--container").removeClass("state");
