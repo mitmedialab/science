7f9fe8f29371dfe58fdfe0ea4a2e002bb3a79097
Kwang-Su Kim <atelier.jullian@gmail.com>
2019-04-17 11:36:24 +0100

    remove unnecessary lines

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,6 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+var gulp = require("gulp");
+var shell = require("gulp-shell");
 
-// Task for building blog when something changed:
-gulp.task('build', shell.task(['bundle exec jekyll serve']));
-// If you don't use bundle:
-// gulp.task('build', shell.task(['jekyll serve']));
-// If you use  Windows Subsystem for Linux (thanks @SamuliAlajarvela):
-// gulp.task('build', shell.task(['bundle exec jekyll serve --force_polling']));
+gulp.task("build", shell.task(["bundle exec jekyll serve"]));
 
-// Task for serving blog with Browsersync
-gulp.task('serve', function () {
-    browserSync.init({ server: { baseDir: '/' } });
-    gulp.watch("scss/*.scss", gulp.series('sass'));
-    // Reloads page when some of the already built files changed:
-    gulp.watch('**/*.*').on('change', browserSync.reload);
-});
-
-gulp.task('sass', function () {
-    return gulp.src("scss/styles.scss").pipe(sass()).pipe(gulp.dest("css/styles.css")).pipe(browserSync.stream());
-});
-
-gulp.task('default', gulp.series('build'));
+gulp.task("default", gulp.series("build"));
